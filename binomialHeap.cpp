adc82f9bb0e32b456a67d9ff3dd8406abad8306f
Leonardo Aravena Cuevas <ld.aravena@gmail.com>
2021-09-28 18:35:10 -0300

    Update Binomial-Heap

--- binomialHeap.cpp
+++ binomialHeap.cpp
@@ -1,1 +1,32 @@
- 
+#include <bits/stdc++.h>
+#include "binomialHeap.cpp"
+
+using namespace std;
+
+binomialHeap::binomialHeap(){
+
+    head = NULL;
+
+}
+
+binomialHeap::insert(int x){
+
+
+}
+
+binomialHeap::getMin(){
+
+    Nodo *y = NULL;
+    Nodo *x = head;
+    int min = INT_MAX;
+
+    while(x != NULL){
+
+        if(x->valor < min){
+
+            min = x->valor;
+            y = x;
+        }
+
+        x = x->sibling;
+}
--- binomialHeap.h
+++ binomialHeap.h
@@ -1,1 +1,39 @@
- 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//  Basado en la información de:
+//  https://www.cs.toronto.edu/~anikolov/CSC265F16/binomial-heaps.pdf
+
+struct Nodo{
+
+    int key;	//¿Es necesario la clave-valor?
+    int valor;
+
+    int degree;
+    Nodo *parent;
+    Nodo *child;
+    Nodo *sibling;
+
+    Nodo(){
+
+        parent = NULL;
+        child = NULL;
+        sibling = NULL;
+    }
+}
+
+class binomialHeap{
+
+private:
+
+    Nodo *head;
+
+public:
+
+    binomialHeap();
+    ~binomialHeap();
+    void insert(int x);
+    int getMin();
+
+}
