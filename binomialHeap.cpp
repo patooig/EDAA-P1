38ad2518750a0f4e7967587dcb8e5541984e962b
patooig <patooig@gmail.com>
2021-10-05 18:52:13 -0300

    Idea en comentarios

--- binomialHeap.cpp
+++ binomialHeap.cpp
@@ -9,10 +9,36 @@ binomialHeap::binomialHeap(){
 
 }
 
-void binomialHeap::insert(int x){
+/*Al momento de insertar debemos crear UN BINOMIAL TREE
+  con el único nodo, asignar grado y comparar el grado
+  de la raiz de los binomial trees.
 
+  Dos binomial trees no pueden tener el mismo grado */
+void binomialHeap::insert(int x){
+    // crear binomial tree
 
 }
+
+/* void createBinomialTree(int x){
+    // Quiza una struct que se llame así
+
+    typedef struct{ 
+        Node * root;
+    }binomialTree
+
+    Node * n_node = new Node();
+    // Asignar valores del nodo, padre,hijo, hermanos..valor.//
+
+    binomialTree * bt1 = new binomialTree();
+    bt1->root = n_node; //o &n_node; ???
+    
+    //Luego comparar con los otros binomialTree, accediendo a la raiz
+    //de cada uno 
+    
+    // Debemos tener un conteo de cuantos binomialTree poseemos, y al momento del merge 
+       se decrementa en 1.
+}
+*/
 
 int binomialHeap::getMin(){
 
