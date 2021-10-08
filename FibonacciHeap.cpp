ab476cfc5fc5efc577ccfb49729ee7a43deea0e1
Ivonnefr <92134679+Ivonnefr@users.noreply.github.com>
2021-10-08 00:06:06 -0300

    Add files via upload

---
+++ FibonacciHeap.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class FibonacciHeap(){
+
+	FibonacciHeap:: FibonacciHeap(){
+		min=NULL;
+	}
+	FibonacciHeap:: ~FibonacciHeap(){
+		//eliminar los nodos
+	}
+	void FibonacciHeap::insert(int n){
+
+		if(min==null){
+			nodo * nuevo = new nodo;
+			nodo->left= nuevo;
+			nodo -> rigth =nuevo;
+			nodo ->parent = NULL;
+			nodo->child =NULL;
+			min=nuevo;
+		}else{
+
+			nodo * nuevo = new nodo;
+			nodo->left= nuevo;
+			nodo -> rigth =nuevo;
+			nodo ->parent = NULL;
+			nodo->child =NULL;
+		}
+	}
+	int FibonacciHeap::getMin(){
+		return min->valor;
+
+	}
+
+}
---
+++ FibonacciHeap.h
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct nodo{
+
+	int valor;
+	nodo * left;
+	nodo * right;
+	nodo * child;
+	nodo * parent;
+	int ranking;
+
+};
+
+class FibonacciHeap{
+
+private:
+	nodo * min;
+
+public:
+	FibonacciHeap();
+	~FibonacciHeap();
+	void insert(int n);
+	int getMin();
+	
+
+}
