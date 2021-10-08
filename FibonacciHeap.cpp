c6b1ef496094fe7b3b0fe7c82cad5cfcf9020ba2
Ivonnefr <ivflores2017@inf.udec.cl>
2021-10-08 00:27:56 -0300

    ola

--- FibonacciHeap.cpp
+++ FibonacciHeap.cpp
@@ -1,32 +1,34 @@
 #include <bits/stdc++.h>
+#include "FibonacciHeap.h"
+
 using namespace std;
 
-// OLA
-	FibonacciHeap:: FibonacciHeap(){
-		min=NULL;
-	}
-	FibonacciHeap:: ~FibonacciHeap(){
-		//eliminar los nodos
-	}
-	void FibonacciHeap::insert(int n){
 
-		if(min==null){
-			nodo * nuevo = new nodo;
-			nodo->left= nuevo;
-			nodo -> rigth =nuevo;
-			nodo ->parent = NULL;
-			nodo->child =NULL;
-			min=nuevo;
-		}else{
+FibonacciHeap::FibonacciHeap(){
+	min=NULL;
+}
+FibonacciHeap:: ~FibonacciHeap(){
+	//eliminar los nodos
+}
+void FibonacciHeap::insert(int n){
 
-			nodo * nuevo = new nodo;
-			nodo->left= nuevo;
-			nodo -> rigth =nuevo;
-			nodo ->parent = NULL;
-			nodo->child =NULL;
-		}
-	}
-	int FibonacciHeap::getMin(){
-		return min->valor;
+	if(min==NULL){
+		nodo * nuevo = new nodo;
+		nodo->left= nuevo;
+		nodo-> rigth =nuevo;
+		nodo->parent = NULL;
+		nodo->child =NULL;
+		min=nuevo;
+	}else{
 
+		nodo * nuevo = new nodo;
+		nodo->left= nuevo;
+		nodo -> rigth =nuevo;
+		nodo ->parent = NULL;
+		nodo->child =NULL;
 	}
+}
+int FibonacciHeap::getMin(){
+	return min->valor;
+
+}
