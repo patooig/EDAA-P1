ae2d74088ab089b89508e881c375f8f07c9273d8
Leonardo Aravena Cuevas <ld.aravena@gmail.com>
2021-10-08 22:58:11 -0300

    main para hacer pruebas

--- mainn.cpp
+++ mainn.cpp
@@ -2,10 +2,51 @@
 #include <iostream>
 #include "minHeap.h"
 #include "FibonacciHeap.h"
+#include "binomialHeap.h"
 
 using namespace std;
 
 int main(){
+
+    clock_t start;
+	double t;
+
+    int n;
+
+    vector<int> v;
+
+    while(cin>>n){
+
+        v.push_back(n);
+    }
+
+    int m = v.size();
+
+    minHeap *mH = new minHeap(m);
+
+    binomialHeap *bH = new binomialHeap();
+
+    start = clock();
+    for(int i=0; i<m; i++){
+
+        mH->insert(v.at(i));
+    }
+    t= (double)(clock()-start)/CLOCKS_PER_SEC;
+
+    cout<<"MinHeap:\n";
+    cout<<t<<endl;
+    cout<<"min: "<<mH->getMin()<<endl;
+
+    start = clock();
+    for(int i=0; i<m; i++){
+
+        bH->insert(v.at(i));
+    }
+    t= (double)(clock()-start)/CLOCKS_PER_SEC;
+
+    cout<<"\nBinomialHeap:\n";
+    cout<<t<<endl;
+    cout<<"min: "<<mH->getMin()<<endl;
 /*
     minHeap * mh = new minHeap(10);
 
@@ -29,7 +70,7 @@ int main(){
     mh->insert(1);
     mh->imprimir();
     delete mh;
-*/
+
     
     FibonacciHeap * fh = new FibonacciHeap();
     fh->insert(10);
@@ -55,5 +96,7 @@ int main(){
     //fhh->imprimir();
    // delete fh;
     delete fhh;
+
+*/
     return 0;
 }
